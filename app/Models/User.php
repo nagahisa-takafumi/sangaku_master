a3e6dad00c17aa695dec0c0acb5e9d922416d3f0
“izawasoma” <izawasoma@gmail.com>
2023-07-02 22:39:34 +0900

    ログイン処理完成

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
+
     /**
      * The attributes that are mass assignable.
      *
